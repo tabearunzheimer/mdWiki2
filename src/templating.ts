cd0dd31cedd8f12c47a021635554f692dd8a34c5
tabearunzheimer <dev.tabea.runzheimer@gmail.com>
2022-05-15 00:12:58 +0200

    fix color changing template

--- src/templating.ts
+++ src/templating.ts
@@ -8,10 +8,10 @@ import * as fs from "fs";
 export function createCustomTemplate(env: Env) {
     const csspath = path.join(__dirname, "..", "templates", "style.css")
     const buffer = fs.readFileSync(csspath);
-    const file = buffer.toString();
+    let file = buffer.toString();
 
     if (env) {
-        checkColorEnvs(file, env);
+        file = checkColorEnvs(file, env);
     }
     fs.writeFileSync(path.join(__dirname, "..", "public", "style.css"), file, 'utf8');
     fs.copyFile(path.join(__dirname, "..", "templates", "loader.js"), path.join(__dirname, "..", "public", "loader.js"), (err) => {
@@ -19,27 +19,27 @@ export function createCustomTemplate(env: Env) {
     });
 }
 
-function checkColorEnvs(file: string, env: Env) {
+function checkColorEnvs(file: string, env: Env) {    
     if (env.MAIN_COLOR && (env.MAIN_COLOR != default_css.main_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_COLOR\s*\*\//g, `${env.MAIN_COLOR}`)
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_COLOR\s*\*\//g, `${env.MAIN_COLOR};`)
     }
     if (env.MAIN_BACKGROUND_COLOR && (env.MAIN_BACKGROUND_COLOR != default_css.main_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_BACKGROUND_COLOR\s*\*\//g, `${env.MAIN_BACKGROUND_COLOR}`)
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_BACKGROUND_COLOR\s*\*\//g, `${env.MAIN_BACKGROUND_COLOR};`)
     }
     if (env.HEADER_BACKGROUND_COLOR && (env.HEADER_BACKGROUND_COLOR != default_css.header_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_BACKGROUND_COLOR\s*\*\//g, `${env.HEADER_BACKGROUND_COLOR}`)
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_BACKGROUND_COLOR\s*\*\//g, `${env.HEADER_BACKGROUND_COLOR};`)
     }
     if (env.HEADER_COLOR && (env.HEADER_COLOR != default_css.header_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_COLOR\s*\*\//g, `${env.HEADER_COLOR}`)
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_COLOR\s*\*\//g, `${env.HEADER_COLOR};`)
     }
     if (env.SIDEBAR_BACKGROUND_COLOR && (env.SIDEBAR_BACKGROUND_COLOR != default_css.sidebar_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_BACKGROUND_COLOR\s*\*\//g, `${env.SIDEBAR_BACKGROUND_COLOR}`)
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_BACKGROUND_COLOR\s*\*\//g, `${env.SIDEBAR_BACKGROUND_COLOR};`)
     }
     if (env.SIDEBAR_COLOR && (env.SIDEBAR_COLOR != default_css.sidebar_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR\s*\*\//g, `${env.SIDEBAR_COLOR}`)
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR\s*\*\//g, `${env.SIDEBAR_COLOR};`)
     }
     if (env.SIDEBAR_COLOR_HOVER && (env.SIDEBAR_COLOR_HOVER != default_css.sidebar_color_hover)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR_HOVER\s*\*\//g, `${env.SIDEBAR_COLOR_HOVER}`)
-    }
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR_HOVER\s*\*\//g, `${env.SIDEBAR_COLOR_HOVER};`)
+    }  
     return file;
 }
