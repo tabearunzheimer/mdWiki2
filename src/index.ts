4c2c86ee2dd4e18e4eb37f10b5850941ac751327
tabearunzheimer <dev.tabea.runzheimer@gmail.com>
2022-09-29 20:16:40 +0200

    fix: environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import express from 'express';
 import { Request, Response } from 'express';
-import { load } from 'ts-dotenv';
+import * as dotenv from 'dotenv';
 import { MdConverter } from './converter';
-import { envSchema } from './models';
 import { startFileWatcher } from './services/observer';
 import { WS } from './services/websocket';
 import { createCustomTemplate } from './templating';
@@ -11,20 +10,19 @@ let path = require('path');
 
 export const root = __dirname;
 
-
-const env = load(envSchema, path.join(__dirname, "..", "sample.env"))
+dotenv.config({path: path.join(__dirname, "..", "sample.env")});
 
 const app = express();
 const server = require('http').createServer(app);
-const port = env.PORT;
+const port = process.env.PORT;
 
-const sourcePath = env.DATA_PATH;
-const renderedPath = env.RENDER_PATH;
+const sourcePath = path.join(process.env.DATA_PATH);
+const renderedPath = process.env.RENDER_PATH;
 
 const ws = new WS(server);
 
-const converter = new MdConverter(sourcePath, renderedPath, ws, env.TITLE, env.LOGO, env.SERVER_IP);
-createCustomTemplate(env);
+const converter = new MdConverter(sourcePath, renderedPath, ws,  process.env.TITLE, process.env.LOGO, process.env.SERVER_IP);
+createCustomTemplate(process.env);
 
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
--- src/templating.ts
+++ src/templating.ts
@@ -1,11 +1,9 @@
-import { EnvType, load } from 'ts-dotenv';
-import { default_css, envSchema } from "./models";
-export type Env = EnvType<typeof envSchema>;
+import { default_css } from "./models";
 
 let path = require('path');
 import * as fs from "fs";
 
-export function createCustomTemplate(env: Env) {
+export function createCustomTemplate(env) {
     const csspath = path.join(__dirname, "..", "templates", "style.css")
     const buffer = fs.readFileSync(csspath);
     let file = buffer.toString();
@@ -22,39 +20,50 @@ export function createCustomTemplate(env: Env) {
     });
 }
 
-function checkColorEnvs(file: string, env: Env) {     
-    if (env.MAIN_COLOR && (env.MAIN_COLOR != default_css.main_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_COLOR\s*\*\//g, `${env.MAIN_COLOR};`)
+function checkColorEnvs(file: string, env) {     
+    const MAIN_COLOR = env.MAIN_COLOR ?? process.env.MAIN_COLOR;
+    const MAIN_BACKGROUND_COLOR = env.MAIN_BACKGROUND_COLOR ?? process.env.MAIN_BACKGROUND_COLOR;
+    const HEADER_BACKGROUND_COLOR = env.MAIN_BACKGROUND_COLOR ?? process.env.HEADER_BACKGROUND_COLOR;
+    const HEADER_COLOR = env.HEADER_COLOR ?? process.env.HEADER_COLOR;
+    const SIDEBAR_BACKGROUND_COLOR = env.SIDEBAR_BACKGROUND_COLOR ?? process.env.SIDEBAR_BACKGROUND_COLOR;
+    const SIDEBAR_COLOR = env.SIDEBAR_COLOR ?? process.env.SIDEBAR_COLOR;
+    const SIDEBAR_COLOR_HOVER = env.SIDEBAR_COLOR_HOVER ?? process.env.SIDEBAR_COLOR_HOVER;
+    const HEADER_CUSTOM_FONT = env.HEADER_CUSTOM_FONT ?? process.env.HEADER_CUSTOM_FONT;
+    const HEADER_CUSTOM_FONT_SIZE = env.HEADER_CUSTOM_FONT_SIZE ?? process.env.HEADER_CUSTOM_FONT_SIZE;
+    const CODE_HIGHLIGHTING_COLOR = env.SIDEBAR_COLOR_HOVER ?? process.env.CODE_HIGHLIGHTING_COLOR;
+
+    if (MAIN_COLOR && (MAIN_COLOR != default_css.main_color)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_COLOR\s*\*\//g, `${MAIN_COLOR};`)
     }
-    if (env.MAIN_BACKGROUND_COLOR && (env.MAIN_BACKGROUND_COLOR != default_css.main_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_BACKGROUND_COLOR\s*\*\//g, `${env.MAIN_BACKGROUND_COLOR};`)
+    if (MAIN_BACKGROUND_COLOR && (MAIN_BACKGROUND_COLOR != default_css.main_background_color)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*MAIN_BACKGROUND_COLOR\s*\*\//g, `${MAIN_BACKGROUND_COLOR};`)
     }
-    if (env.HEADER_BACKGROUND_COLOR && (env.HEADER_BACKGROUND_COLOR != default_css.header_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_BACKGROUND_COLOR\s*\*\//g, `${env.HEADER_BACKGROUND_COLOR};`)
+    if (HEADER_BACKGROUND_COLOR && (HEADER_BACKGROUND_COLOR != default_css.header_background_color)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_BACKGROUND_COLOR\s*\*\//g, `${HEADER_BACKGROUND_COLOR};`)
     }
-    if (env.HEADER_COLOR && (env.HEADER_COLOR != default_css.header_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_COLOR\s*\*\//g, `${env.HEADER_COLOR};`)
+    if (HEADER_COLOR && (HEADER_COLOR != default_css.header_background_color)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*HEADER_COLOR\s*\*\//g, `${HEADER_COLOR};`)
     }
-    if (env.SIDEBAR_BACKGROUND_COLOR && (env.SIDEBAR_BACKGROUND_COLOR != default_css.sidebar_background_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_BACKGROUND_COLOR\s*\*\//g, `${env.SIDEBAR_BACKGROUND_COLOR};`)
+    if (SIDEBAR_BACKGROUND_COLOR && (SIDEBAR_BACKGROUND_COLOR != default_css.sidebar_background_color)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_BACKGROUND_COLOR\s*\*\//g, `${SIDEBAR_BACKGROUND_COLOR};`)
     }
-    if (env.SIDEBAR_COLOR && (env.SIDEBAR_COLOR != default_css.sidebar_color)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR\s*\*\//g, `${env.SIDEBAR_COLOR};`)
+    if (SIDEBAR_COLOR && (SIDEBAR_COLOR != default_css.sidebar_color)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR\s*\*\//g, `${SIDEBAR_COLOR};`)
     }
-    if (env.SIDEBAR_COLOR_HOVER && (env.SIDEBAR_COLOR_HOVER != default_css.sidebar_color_hover)) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR_HOVER\s*\*\//g, `${env.SIDEBAR_COLOR_HOVER};`)
+    if (SIDEBAR_COLOR_HOVER && (SIDEBAR_COLOR_HOVER != default_css.sidebar_color_hover)) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*SIDEBAR_COLOR_HOVER\s*\*\//g, `${SIDEBAR_COLOR_HOVER};`)
     }  
-    if (env.HEADER_CUSTOM_FONT) {
-        fs.copyFile(path.join(__dirname, "..", env.HEADER_CUSTOM_FONT), path.join(__dirname, "..", "public", env.HEADER_CUSTOM_FONT), (err) => {
+    if (HEADER_CUSTOM_FONT) {
+        fs.copyFile(path.join(__dirname, "..", HEADER_CUSTOM_FONT), path.join(__dirname, "..", "public",HEADER_CUSTOM_FONT), (err) => {
             console.error(err);
         });
-        file = file.replace(/REPLACE_FONT_PATH/g, `./${env.HEADER_CUSTOM_FONT}`)
+        file = file.replace(/REPLACE_FONT_PATH/g, `./${HEADER_CUSTOM_FONT}`)
     }
-    if (env.HEADER_CUSTOM_FONT_SIZE) {        
-        file = file.replace(/\d+px\s*\;\s*\/\*\s*HEADER_CUSTOM_FONT_SIZE\s*\*\//g, `${env.HEADER_CUSTOM_FONT_SIZE};`)
+    if (HEADER_CUSTOM_FONT_SIZE) {        
+        file = file.replace(/\d+px\s*\;\s*\/\*\s*HEADER_CUSTOM_FONT_SIZE\s*\*\//g, `${HEADER_CUSTOM_FONT_SIZE};`)
     }
-    if (env.CODE_HIGHLIGHTING_COLOR) {
-        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*CODE_HIGHLIGHTING_COLOR\s*\*\//g, `${env.CODE_HIGHLIGHTING_COLOR};`)
+    if (CODE_HIGHLIGHTING_COLOR) {
+        file = file.replace(/\#[a-fA-F0-9]{6}\s*\;\s*\/\*\s*CODE_HIGHLIGHTING_COLOR\s*\*\//g, `${CODE_HIGHLIGHTING_COLOR};`)
     }    
     return file;
 }
